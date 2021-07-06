260f50191c9ebd186d911c83a8f9eee1f6612e23
sushma758 <78887242+sushma758@users.noreply.github.com>
2021-07-06 13:27:44 -0400

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -3,6 +3,6 @@ function addUser(){
     player2_name=document.getElementById("player2_name_input").Value;
     localStorage.setItem("player1_name", player1_name);
     localStorage.setItem("player2_name", player2_name);
-    window.location="game_page.html";
+    window.location="game_page.js";
     
 }
